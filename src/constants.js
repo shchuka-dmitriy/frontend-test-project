79d4ce2f2a67f12dd89030c368da52fcf034df91
shchuka-dmitriy <dmitriy.shchuka.rule@gmail.com>
2020-08-19 06:28:33 +0300

    Create constants and common styles

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export default {
+  BASE_URL: `http://smktesting.herokuapp.com/`,
+  STATIC_IMAGES_PATH: '/staticImages/',
+  ANONYM_IMAGE_PATH: '/staticImages/default-image.jpg',
+  ACCESS_TOKEN: 'accessToken',
+  USER_IN_STORAGE: 'user'
+};
