a293659d1c7be802d6885f838560876d2a14e12e
shchuka-dmitriy <dmitriy.shchuka.rule@gmail.com>
2020-08-19 06:22:01 +0300

    Create Home page and change App:
      - add index.js
      - add Home.js
      - change App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Router, Route, Switch} from 'react-router-dom';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
+import ProductPage from './pages/ProductPage/ProductPage';
+import NotFound from './components/NotFound/NotFound';
+import Home from './pages/Home/Home';
+import browserHistory from './browserHistory';
 import './App.css';
+import './reset.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <Router history={browserHistory}>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/login' component={LoginPage}/>
+            <Route exact path='/registration' component={RegistrationPage}/>
+            <Route exact path='/product/:id' component={ProductPage}/>
+            <Route component={NotFound}/>
+          </Switch>
+        </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import ProductsList from "../../components/ProductsList/ProductsList";
+
+const Home = () => {
+    return (
+        <>
+            <Header/>
+            <ProductsList/>
+        </>
+    )
+};
+
+export default Home;
